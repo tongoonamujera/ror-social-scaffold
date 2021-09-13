0642e7ca1a3b4429a1bbd796bbba549166aa8a87
tongoonamujera <tongoonamujera@gmail.com>
2021-09-13 08:02:26 +0200

    Fix linter errors #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships, dependent: :destroy
-  has_many :friends, ->{ merge(Friendship.confirmed_friendship) }, through: :friendships
+  has_many :friends, -> { merge(Friendship.confirmed_friendship) }, through: :friendships
 
   def add_friend(another_user)
     friends << another_user
