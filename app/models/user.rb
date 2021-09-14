dbcee3e0c4846f135f683d3c773847e083eb9855
tongoonamujera <tongoonamujera@gmail.com>
2021-09-14 22:15:10 +0200

    Remove unneccessary code in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,17 +20,6 @@ class User < ApplicationRecord
   # Users who requested to be friends (needed for notifications)
   has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships, source: :friend
-#rubocop:disable all
-  def friends_and_own_posts
-    Post.where(user: (self.friends.to_a << self))
-    # This will produce SQL query with IN. Something like: select * from posts where user_id IN (1,45,874,43);
-  end
-
 
   has_many :friendships, dependent: :destroy
-  # has_many :friends, -> { merge(Friendship.confirmed_friendship) }, through: :friendships
-
-  def add_friend(another_user)
-    friends << another_user
-  end
 end
